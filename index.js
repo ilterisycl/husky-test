fc29a0b9a8575dac383324c4d15ce5fa882b3d9f
ilterisycl <ilterisyucel@firatlifestyle.com>
2025-08-20 11:46:42 +0300

    Test

---
+++ index.js
@@ -0,0 +1,2 @@
+const x = 5 *4;
+const y = 10 * 2;
---
+++ logs.js
@@ -0,0 +1,7 @@
+console.log("Warn me, there is a console.log here");
+console.warn("This is a warning message");
+console.error("This is an error message");
+console.info("This is an info message");
+console.debug("This is a debug message");
+console.trace("This is a trace message");
+console.assert(1 === 1, "This assertion will not fail");
