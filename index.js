1ce61471e8a113f2c2684b0312ee66d3bf22722e
ilterisycl <ilterisyucel@firatlifestyle.com>
2025-08-20 12:28:07 +0300

    Test

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const x = 5 *4;
 const y = 10 * 2;
+confole.log("This is a warning message");
+console.warn("This is a warning message");
+console.error("This is an error message");
--- logs.js
+++ logs.js
@@ -3,3 +3,5 @@ y = 10 * 2;
 // This is a sample code snippet to demonstrate the removal of console logs.
 // The console.log statements should be removed by the script.
 // The above console logs should not appear in the final output after running the script.
+x = 5 * 4;
+y = 10 * 2;
