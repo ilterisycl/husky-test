219d50f8ef1fb07b2de0fb2bbb0803102d7a670a
ilterisycl <ilterisyucel@firatlifestyle.com>
2025-08-21 09:40:30 +0300

    feat: Fix

--- logs.js
+++ logs.js
@@ -1,7 +1,5 @@
 x = 5 * 4;
 y = 10 * 2;
-// This is a sample code snippet to demonstrate the removal of console logs.
-// The console.log statements should be removed by the script.
-// The above console logs should not appear in the final output after running the script.
+console.log("This is a warning message");
 x = 5 * 4;
 y = 10 * 2;
