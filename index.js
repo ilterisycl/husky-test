543897dbac5337e9e69c97a5f1fc50e714c624ca
ilterisycl <ilterisyucel@firatlifestyle.com>
2025-08-21 09:48:28 +0300

    feat: Fix

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const x = 5 *4;
 const y = 10 * 2;
 confole.log("This is a warning message");
+console.error("This is an error message");
+console.warn("This is a warning message");
+console.log("############################################ Uzun Console Log ############################################");
--- logs.js
+++ logs.js
@@ -1,7 +1,4 @@
 x = 5 * 4;
 y = 10 * 2;
-console.log("This is a warning message");
 x = 5 * 4;
 y = 10 * 2;
-console.error("This is an error message");
-console.log("This is a warning message");
